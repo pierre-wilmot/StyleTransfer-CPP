049701cf1f861b399057a39beee504947a1d8221
Pierre Wilmot <pierre.wilmot@gmail.com>
2019-06-23 15:23:06 +0200

    Disable CUDA for the time being

--- download_torch.sh
+++ download_torch.sh
@@ -1,3 +1,3 @@
 mkdir lib ; cd lib
-wget https://download.pytorch.org/libtorch/nightly/cu90/libtorch-shared-with-deps-latest.zip
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
