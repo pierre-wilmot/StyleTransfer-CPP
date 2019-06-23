4e449e36cdcb1eca076cbb33cd504ee1544e9ef7
Pierre WILMOT <gheaeckkseqrz@gmail.com>
2019-06-23 14:38:22 +0100

    Import libtorch in the project (#2)
    * Import libtorch in the project
    
    * Disable CUDA for the time being

---
+++ download_torch.sh
@@ -0,0 +1,3 @@
+mkdir lib ; cd lib
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <torch/torch.h>
 
 int main(int ac, char **av)
 {
   std::cout << "StyleTransfer++" << std::endl;
+
+  torch::Tensor t = torch::zeros({5, 5});
+  std::cout << t << std::endl;
+  
   return 0;
 }
