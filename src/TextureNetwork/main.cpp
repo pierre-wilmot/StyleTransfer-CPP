fd54986acf13558fe3f0fad6579fb4ebd481bab8
Pierre Wilmot <pierre.wilmot@gmail.com>
2021-03-06 22:30:47 +0000

    Increase network capacity

--- src/TextureNetwork/main.cpp
+++ src/TextureNetwork/main.cpp
@@ -6,17 +6,55 @@
 
 constexpr unsigned int SAMPLES = 10000;
 
+class ResidualBlockImpl : public torch::nn::Module
+{
+public:
+  ResidualBlockImpl(unsigned int nc = 64)
+  {
+    _c1 = register_module("c1", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c2 = register_module("c2", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c3 = register_module("c3", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c4 = register_module("c4", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c5 = register_module("c5", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c6 = register_module("c6", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c7 = register_module("c7", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+  }
+
+  torch::Tensor forward(torch::Tensor const &x_)
+  {
+    torch::Tensor x = x_;
+    x = torch::relu(_c1(x));
+    x = torch::relu(_c2(x));
+    x = torch::relu(_c3(x));
+    x = torch::relu(_c4(x));
+    x = torch::relu(_c5(x));
+    x = torch::relu(_c6(x));
+    x = _c7(x);
+    return x + x_;
+  }
+
+private:
+  torch::nn::Conv2d _c1 = nullptr;
+  torch::nn::Conv2d _c2 = nullptr;
+  torch::nn::Conv2d _c3 = nullptr;
+  torch::nn::Conv2d _c4 = nullptr;
+  torch::nn::Conv2d _c5 = nullptr;
+  torch::nn::Conv2d _c6 = nullptr;
+  torch::nn::Conv2d _c7 = nullptr;
+};
+TORCH_MODULE(ResidualBlock);
+
 class GeneratorImpl : public torch::nn::Module
 {
 public:
   GeneratorImpl(unsigned int nc = 64)
   {
     _c1 = register_module("c1", torch::nn::Conv2d(torch::nn::Conv2dOptions(3, nc, 3).padding(1)));
-    _c2 = register_module("c2", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
-    _c3 = register_module("c3", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
-    _c4 = register_module("c4", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
-    _c5 = register_module("c5", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
-    _c6 = register_module("c6", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, nc, 3).padding(1)));
+    _c2 = register_module("c2", ResidualBlock(64));
+    _c3 = register_module("c3", ResidualBlock(64));
+    _c4 = register_module("c4", ResidualBlock(64));
+    _c5 = register_module("c5", ResidualBlock(64));
+    _c6 = register_module("c6", ResidualBlock(64));
     _c7 = register_module("c7", torch::nn::Conv2d(torch::nn::Conv2dOptions(nc, 3, 3).padding(1)));
   }
 
@@ -35,11 +73,11 @@ public:
 
 private:
   torch::nn::Conv2d _c1 = nullptr;
-  torch::nn::Conv2d _c2 = nullptr;
-  torch::nn::Conv2d _c3 = nullptr;
-  torch::nn::Conv2d _c4 = nullptr;
-  torch::nn::Conv2d _c5 = nullptr;
-  torch::nn::Conv2d _c6 = nullptr;
+  ResidualBlock _c2 = nullptr;
+  ResidualBlock _c3 = nullptr;
+  ResidualBlock _c4 = nullptr;
+  ResidualBlock _c5 = nullptr;
+  ResidualBlock _c6 = nullptr;
   torch::nn::Conv2d _c7 = nullptr;
 };
 TORCH_MODULE(Generator);
